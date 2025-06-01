b655d339d1be0314db6d8b6a6fa2a9240fc31275
Hemantdahiyacodes <hemantdahiyacodes@gmail.com>
2025-06-01 09:16:04 +0530

    Added loggin and logout form

--- app.js
+++ app.js
@@ -1,39 +1,95 @@
 const path = require("node:path");
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 
 const pool = new Pool({
-    host: process.env.host,
-    user: process.env.user,
-    database: process.env.database,
-    password: process.env.password,
-    port: Number(process.env.port),
-})
+  host: process.env.host,
+  user: process.env.user,
+  database: process.env.database,
+  password: process.env.password,
+  port: Number(process.env.port),
+});
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        "SELECT * FROM users WHERE username = $1",
+        [username]
+      );
+      const user = rows[0];
 
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
 
-app.use(session({secret: "cats", resave: "false", saveUninitialized: false}));
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect Password" });
+      }
+
+      return done(user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.use(session({ secret: "cats", resave: "false", saveUninitialized: false }));
 app.use(passport.session());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.render("index", { user: req.user }));
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+
+app.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return err;
+    }
+
+    res.redirect("/");
+  });
+});
 app.get("/sign-up", (req, res) => res.render("signup-form"));
 app.post("/sign-up", async (req, res, next) => {
-    try {
-        await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [req.body.username, req.body.password]);
-        res.redirect("/");
-    }
+  try {
+    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+      req.body.username,
+      req.body.password,
+    ]);
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
+});
 
-    catch (err) {
-        return next(err);
-    }
-})
-app.listen(process.env.port || 8000);
+app.listen(8000);
